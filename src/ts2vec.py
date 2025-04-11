21cf82a50993f4cadc8f08b49febafd3b03be8cb
Cibber-2 <carlos.laborda.g@gmail.com>
2025-04-11 08:58:05 +0200

    add utility functions for data handling and model initialization in ts2vec

--- src/ts2vec.py
+++ src/ts2vec.py
@@ -1,12 +1,317 @@
+import os
 import torch
+from torch import nn
 import torch.nn.functional as F
 from torch.utils.data import TensorDataset, DataLoader
 import numpy as np
-from models import TSEncoder
-from models.losses import hierarchical_contrastive_loss
-from utils import take_per_row, split_with_nan, centerize_vary_length_series, torch_pad_nan
 import math
+import random
+from datetime import datetime
+import pickle
 
+# --------------------------------------------------------
+# utils.py
+# --------------------------------------------------------
+def pkl_save(name, var):
+    with open(name, 'wb') as f:
+        pickle.dump(var, f)
+
+def pkl_load(name):
+    with open(name, 'rb') as f:
+        return pickle.load(f)
+    
+def torch_pad_nan(arr, left=0, right=0, dim=0):
+    if left > 0:
+        padshape = list(arr.shape)
+        padshape[dim] = left
+        arr = torch.cat((torch.full(padshape, np.nan), arr), dim=dim)
+    if right > 0:
+        padshape = list(arr.shape)
+        padshape[dim] = right
+        arr = torch.cat((arr, torch.full(padshape, np.nan)), dim=dim)
+    return arr
+    
+def pad_nan_to_target(array, target_length, axis=0, both_side=False):
+    assert array.dtype in [np.float16, np.float32, np.float64]
+    pad_size = target_length - array.shape[axis]
+    if pad_size <= 0:
+        return array
+    npad = [(0, 0)] * array.ndim
+    if both_side:
+        npad[axis] = (pad_size // 2, pad_size - pad_size//2)
+    else:
+        npad[axis] = (0, pad_size)
+    return np.pad(array, pad_width=npad, mode='constant', constant_values=np.nan)
+
+def split_with_nan(x, sections, axis=0):
+    assert x.dtype in [np.float16, np.float32, np.float64]
+    arrs = np.array_split(x, sections, axis=axis)
+    target_length = arrs[0].shape[axis]
+    for i in range(len(arrs)):
+        arrs[i] = pad_nan_to_target(arrs[i], target_length, axis=axis)
+    return arrs
+
+def take_per_row(A, indx, num_elem):
+    all_indx = indx[:,None] + np.arange(num_elem)
+    return A[torch.arange(all_indx.shape[0])[:,None], all_indx]
+
+def centerize_vary_length_series(x):
+    prefix_zeros = np.argmax(~np.isnan(x).all(axis=-1), axis=1)
+    suffix_zeros = np.argmax(~np.isnan(x[:, ::-1]).all(axis=-1), axis=1)
+    offset = (prefix_zeros + suffix_zeros) // 2 - prefix_zeros
+    rows, column_indices = np.ogrid[:x.shape[0], :x.shape[1]]
+    offset[offset < 0] += x.shape[1]
+    column_indices = column_indices - offset[:, np.newaxis]
+    return x[rows, column_indices]
+
+def data_dropout(arr, p):
+    B, T = arr.shape[0], arr.shape[1]
+    mask = np.full(B*T, False, dtype=np.bool)
+    ele_sel = np.random.choice(
+        B*T,
+        size=int(B*T*p),
+        replace=False
+    )
+    mask[ele_sel] = True
+    res = arr.copy()
+    res[mask.reshape(B, T)] = np.nan
+    return res
+
+def name_with_datetime(prefix='default'):
+    now = datetime.now()
+    return prefix + '_' + now.strftime("%Y%m%d_%H%M%S")
+
+def init_dl_program(
+    device_name,
+    seed=None,
+    use_cudnn=True,
+    deterministic=False,
+    benchmark=False,
+    use_tf32=False,
+    max_threads=None
+):
+    import torch
+    if max_threads is not None:
+        torch.set_num_threads(max_threads)  # intraop
+        if torch.get_num_interop_threads() != max_threads:
+            torch.set_num_interop_threads(max_threads)  # interop
+        try:
+            import mkl
+        except:
+            pass
+        else:
+            mkl.set_num_threads(max_threads)
+        
+    if seed is not None:
+        random.seed(seed)
+        seed += 1
+        np.random.seed(seed)
+        seed += 1
+        torch.manual_seed(seed)
+        
+    if isinstance(device_name, (str, int)):
+        device_name = [device_name]
+    
+    devices = []
+    for t in reversed(device_name):
+        t_device = torch.device(t)
+        devices.append(t_device)
+        if t_device.type == 'cuda':
+            assert torch.cuda.is_available()
+            torch.cuda.set_device(t_device)
+            if seed is not None:
+                seed += 1
+                torch.cuda.manual_seed(seed)
+    devices.reverse()
+    torch.backends.cudnn.enabled = use_cudnn
+    torch.backends.cudnn.deterministic = deterministic
+    torch.backends.cudnn.benchmark = benchmark
+    
+    if hasattr(torch.backends.cudnn, 'allow_tf32'):
+        torch.backends.cudnn.allow_tf32 = use_tf32
+        torch.backends.cuda.matmul.allow_tf32 = use_tf32
+        
+    return devices if len(devices) > 1 else devices[0]
+
+
+# --------------------------------------------------------
+# losses.py
+# --------------------------------------------------------
+def hierarchical_contrastive_loss(z1, z2, alpha=0.5, temporal_unit=0):
+    loss = torch.tensor(0., device=z1.device)
+    d = 0
+    while z1.size(1) > 1:
+        if alpha != 0:
+            loss += alpha * instance_contrastive_loss(z1, z2)
+        if d >= temporal_unit:
+            if 1 - alpha != 0:
+                loss += (1 - alpha) * temporal_contrastive_loss(z1, z2)
+        d += 1
+        z1 = F.max_pool1d(z1.transpose(1, 2), kernel_size=2).transpose(1, 2)
+        z2 = F.max_pool1d(z2.transpose(1, 2), kernel_size=2).transpose(1, 2)
+    if z1.size(1) == 1:
+        if alpha != 0:
+            loss += alpha * instance_contrastive_loss(z1, z2)
+        d += 1
+    return loss / d
+
+def instance_contrastive_loss(z1, z2):
+    B, T = z1.size(0), z1.size(1)
+    if B == 1:
+        return z1.new_tensor(0.)
+    z = torch.cat([z1, z2], dim=0)  # 2B x T x C
+    z = z.transpose(0, 1)  # T x 2B x C
+    sim = torch.matmul(z, z.transpose(1, 2))  # T x 2B x 2B
+    logits = torch.tril(sim, diagonal=-1)[:, :, :-1]    # T x 2B x (2B-1)
+    logits += torch.triu(sim, diagonal=1)[:, :, 1:]
+    logits = -F.log_softmax(logits, dim=-1)
+    
+    i = torch.arange(B, device=z1.device)
+    loss = (logits[:, i, B + i - 1].mean() + logits[:, B + i, i].mean()) / 2
+    return loss
+
+def temporal_contrastive_loss(z1, z2):
+    B, T = z1.size(0), z1.size(1)
+    if T == 1:
+        return z1.new_tensor(0.)
+    z = torch.cat([z1, z2], dim=1)  # B x 2T x C
+    sim = torch.matmul(z, z.transpose(1, 2))  # B x 2T x 2T
+    logits = torch.tril(sim, diagonal=-1)[:, :, :-1]    # B x 2T x (2T-1)
+    logits += torch.triu(sim, diagonal=1)[:, :, 1:]
+    logits = -F.log_softmax(logits, dim=-1)
+    
+    t = torch.arange(T, device=z1.device)
+    loss = (logits[:, t, T + t - 1].mean() + logits[:, T + t, t].mean()) / 2
+    return loss
+
+# --------------------------------------------------------
+# dilated_conv.py
+# --------------------------------------------------------
+class SamePadConv(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, dilation=1, groups=1):
+        super().__init__()
+        self.receptive_field = (kernel_size - 1) * dilation + 1
+        padding = self.receptive_field // 2
+        self.conv = nn.Conv1d(
+            in_channels, out_channels, kernel_size,
+            padding=padding,
+            dilation=dilation,
+            groups=groups
+        )
+        self.remove = 1 if self.receptive_field % 2 == 0 else 0
+        
+    def forward(self, x):
+        out = self.conv(x)
+        if self.remove > 0:
+            out = out[:, :, : -self.remove]
+        return out
+    
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, dilation, final=False):
+        super().__init__()
+        self.conv1 = SamePadConv(in_channels, out_channels, kernel_size, dilation=dilation)
+        self.conv2 = SamePadConv(out_channels, out_channels, kernel_size, dilation=dilation)
+        self.projector = nn.Conv1d(in_channels, out_channels, 1) if in_channels != out_channels or final else None
+    
+    def forward(self, x):
+        residual = x if self.projector is None else self.projector(x)
+        x = F.gelu(x)
+        x = self.conv1(x)
+        x = F.gelu(x)
+        x = self.conv2(x)
+        return x + residual
+    
+class DilatedConvEncoder(nn.Module):
+    def __init__(self, in_channels, channels, kernel_size):
+        super().__init__()
+        self.net = nn.Sequential(*[
+            ConvBlock(
+                channels[i-1] if i > 0 else in_channels,
+                channels[i],
+                kernel_size=kernel_size,
+                dilation=2**i,
+                final=(i == len(channels)-1)
+            )
+            for i in range(len(channels))
+        ])
+        
+    def forward(self, x):
+        return self.net(x)
+
+# --------------------------------------------------------
+# encoder.py
+# --------------------------------------------------------
+def generate_continuous_mask(B, T, n=5, l=0.1):
+    res = torch.full((B, T), True, dtype=torch.bool)
+    if isinstance(n, float):
+        n = int(n * T)
+    n = max(min(n, T // 2), 1)
+    
+    if isinstance(l, float):
+        l = int(l * T)
+    l = max(l, 1)
+    
+    for i in range(B):
+        for _ in range(n):
+            t = np.random.randint(T-l+1)
+            res[i, t:t+l] = False
+    return res
+
+def generate_binomial_mask(B, T, p=0.5):
+    return torch.from_numpy(np.random.binomial(1, p, size=(B, T))).to(torch.bool)
+
+class TSEncoder(nn.Module):
+    def __init__(self, input_dims, output_dims, hidden_dims=64, depth=10, mask_mode='binomial'):
+        super().__init__()
+        self.input_dims = input_dims
+        self.output_dims = output_dims
+        self.hidden_dims = hidden_dims
+        self.mask_mode = mask_mode
+        self.input_fc = nn.Linear(input_dims, hidden_dims)
+        self.feature_extractor = DilatedConvEncoder(
+            hidden_dims,
+            [hidden_dims] * depth + [output_dims],
+            kernel_size=3
+        )
+        self.repr_dropout = nn.Dropout(p=0.1)
+        
+    def forward(self, x, mask=None):  # x: B x T x input_dims
+        nan_mask = ~x.isnan().any(axis=-1)
+        x[~nan_mask] = 0
+        x = self.input_fc(x)  # B x T x Ch
+        
+        # generate & apply mask
+        if mask is None:
+            if self.training:
+                mask = self.mask_mode
+            else:
+                mask = 'all_true'
+        
+        if mask == 'binomial':
+            mask = generate_binomial_mask(x.size(0), x.size(1)).to(x.device)
+        elif mask == 'continuous':
+            mask = generate_continuous_mask(x.size(0), x.size(1)).to(x.device)
+        elif mask == 'all_true':
+            mask = x.new_full((x.size(0), x.size(1)), True, dtype=torch.bool)
+        elif mask == 'all_false':
+            mask = x.new_full((x.size(0), x.size(1)), False, dtype=torch.bool)
+        elif mask == 'mask_last':
+            mask = x.new_full((x.size(0), x.size(1)), True, dtype=torch.bool)
+            mask[:, -1] = False
+        
+        mask &= nan_mask
+        x[~mask] = 0
+        
+        # conv encoder
+        x = x.transpose(1, 2)  # B x Ch x T
+        x = self.repr_dropout(self.feature_extractor(x))  # B x Co x T
+        x = x.transpose(1, 2)  # B x T x Co
+        
+        return x
+        
+# --------------------------------------------------------
+# ts2vec.py
+# --------------------------------------------------------
 class TS2Vec:
     '''The TS2Vec model'''
     
