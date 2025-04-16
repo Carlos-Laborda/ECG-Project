44796714e11e5d9a9056a33f653216069712fa82
Cibber-2 <carlos.laborda.g@gmail.com>
2025-04-16 17:24:52 +0200

    add label_fraction argument to ts2vec training script for data labeling control

--- src/train.sh
+++ src/train.sh
@@ -32,4 +32,5 @@ export MLFLOW_TRACKING_URI=http://fs0.das6.cs.vu.nl:5005
 python ts2vec_train.py run \
   --mlflow_tracking_uri "http://fs0.das6.cs.vu.nl:5005" \
   --window_data_path "../../../../var/scratch/cla224/ECG-Project/data/windowed_data.h5" \
-  --ts2vec_epochs 10
+  --ts2vec_epochs 10 \
+  --label_fraction 1.0
