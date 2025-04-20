864a2b9e247cbc844103c2297ea9ebccb0c7dcf8
Cibber-2 <carlos.laborda.g@gmail.com>
2025-04-20 09:12:04 +0200

    update training script to use ts2vec_soft_train.py with reduced epochs

--- src/train.sh
+++ src/train.sh
@@ -29,8 +29,8 @@ export MLFLOW_TRACKING_URI=http://fs0.das6.cs.vu.nl:5005
 #   --num_epochs 20
 
 # --- Run Self Supervised Model script ---
-python ts2vec_train.py run \
+python ts2vec_soft_train.py run \
   --mlflow_tracking_uri "http://fs0.das6.cs.vu.nl:5005" \
   --window_data_path "../../../../var/scratch/cla224/ECG-Project/data/windowed_data.h5" \
-  --ts2vec_epochs 10 \
+  --ts2vec_epochs 3 \
   --label_fraction 1.0
