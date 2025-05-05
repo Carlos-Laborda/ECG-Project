c17d047158a2e86eb69a35604906c6c82b57d2cd
Cibber-2 <carlos.laborda.g@gmail.com>
2025-05-05 20:18:03 +0200

    refactor model class name and enhance forward method for soft instance and temporal learning

--- src/tstcc_soft.py
+++ src/tstcc_soft.py
@@ -524,9 +524,9 @@ class TC(nn.Module):
 # ----------------------------------------------------------------------
 # model.py
 # ----------------------------------------------------------------------
-class base_Model(nn.Module):
+class tstcc_soft(nn.Module):
     def __init__(self, configs):
-        super(base_Model, self).__init__()
+        super(tstcc_soft, self).__init__()
 
         self.conv_block1 = nn.Sequential(
             nn.Conv1d(configs.input_channels, 32, kernel_size=configs.kernel_size,
@@ -553,18 +553,100 @@ class base_Model(nn.Module):
 
         model_output_dim = configs.features_len
         self.logits = nn.Linear(model_output_dim * configs.final_out_channels, configs.num_classes)
+        self.lambda_ = configs.lambda_ # 0.5
+        self.soft_temporal = configs.soft_temporal #True
+        self.soft_instance = configs.soft_instance #True
+        
+        self.tau_temp = configs.tau_temp
 
-    def forward(self, x_in):
-        show_shape("base_Model in", x_in)
-        x = self.conv_block1(x_in)
-        x = self.conv_block2(x)
-        x = self.conv_block3(x)
+    def forward(self, aug1, aug2, soft_labels, train=True):
+        if train:
+            if self.soft_instance:
+                soft_labels_L, soft_labels_R = dup_matrix(soft_labels)
+                del soft_labels
+            
+            temporal_loss = torch.tensor(0., device=aug1.device)
+            instance_loss = torch.tensor(0., device=aug1.device)
+            
+            #-------------------------------------------------#
+            # DEPTH = 1
+            #-------------------------------------------------#
+            aug1 = self.conv_block1(aug1)
+            aug2 = self.conv_block1(aug2)
+            if self.soft_temporal:
+                d=0
+                timelag = timelag_sigmoid(aug1.shape[2], self.tau_temp*(2**d))
+                timelag = torch.tensor(timelag, device=aug1.device)
+                timelag_L, timelag_R = dup_matrix(timelag)
+                temporal_loss += (1-self.lambda_) * temp_CL_soft(aug1, aug2, timelag_L, timelag_R)
+            else:
+                temporal_loss += (1-self.lambda_) * temp_CL_hard(aug1, aug2)
+            if self.soft_instance:
+                instance_loss += self.lambda_ * inst_CL_soft(aug1, aug2, soft_labels_L, soft_labels_R)
+            else:
+                instance_loss += self.lambda_ * inst_CL_hard(aug1, aug2)
 
-        x_flat = x.reshape(x.shape[0], -1)
-        show_shape("base_Model feat_out(flat)", x_flat)
-        logits = self.logits(x_flat)
-        return logits, x
-    
+            #-------------------------------------------------#
+            # DEPTH = 2
+            #-------------------------------------------------#
+            aug1 = self.conv_block2(aug1)
+            aug2 = self.conv_block2(aug2)
+
+            if self.soft_temporal:
+                d=1
+                timelag = timelag_sigmoid(aug1.shape[2],self.tau_temp*(2**d))
+                timelag = torch.tensor(timelag, device=aug1.device)
+                timelag_L, timelag_R = dup_matrix(timelag)
+                temporal_loss += (1-self.lambda_) * temp_CL_soft(aug1, aug2, timelag_L, timelag_R)
+            else:
+                temporal_loss += (1-self.lambda_) * temp_CL_hard(aug1, aug2)
+                
+            if self.soft_instance:
+                instance_loss += self.lambda_ * inst_CL_soft(aug1, aug2, soft_labels_L, soft_labels_R)
+            else:
+                instance_loss += self.lambda_ * inst_CL_hard(aug1, aug2)
+            
+            #-------------------------------------------------#
+            # DEPTH = 3
+            #-------------------------------------------------#
+            aug1 = self.conv_block3(aug1)
+            aug2 = self.conv_block3(aug2)
+        
+            if self.soft_temporal:
+                d=2
+                timelag = timelag_sigmoid(aug1.shape[2],self.tau_temp*(2**d))
+                timelag = torch.tensor(timelag, device=aug1.device)
+                timelag_L, timelag_R = dup_matrix(timelag)
+                temporal_loss += (1-self.lambda_) * temp_CL_soft(aug1, aug2, timelag_L, timelag_R)
+            else:
+                temporal_loss += (1-self.lambda_) * temp_CL_hard(aug1, aug2)
+           
+            if self.soft_instance:
+                instance_loss += self.lambda_ * inst_CL_soft(aug1, aug2, soft_labels_L, soft_labels_R)
+                del soft_labels_L, soft_labels_R
+            else:
+                instance_loss += self.lambda_ * inst_CL_hard(aug1, aug2)
+        
+        else:
+            aug = self.conv_block1(aug1)
+            aug = self.conv_block2(aug)
+            aug = self.conv_block3(aug)
+        
+        ############################################################################
+        ############################################################################
+        
+        if train:
+            aug1_flat = aug1.reshape(aug1.shape[0], -1)
+            aug2_flat = aug2.reshape(aug2.shape[0], -1)
+            aug1_logits = self.logits(aug1_flat)
+            aug2_logits = self.logits(aug2_flat)
+            final_loss = temporal_loss + instance_loss
+            return aug1_logits, aug2_logits, aug1, aug2, final_loss
+
+        else:
+            aug_flat = aug.reshape(aug.shape[0], -1)
+            aug_logits = self.logits(aug_flat)
+            return aug_logits, aug
 
 # ----------------------------------------------------------------------
 # utils.py
