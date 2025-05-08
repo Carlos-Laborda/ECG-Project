4455bbfe1b7570b4530b15217d80831645e3afcf
Cibber-2 <carlos.laborda.g@gmail.com>
2025-05-08 11:18:52 +0200

    update training script to run tstcc_soft_train with adjusted parameters

--- src/train.sh
+++ src/train.sh
@@ -29,12 +29,18 @@ export MLFLOW_TRACKING_URI=http://fs0.das6.cs.vu.nl:5005
 #   --num_epochs 20
 
 # --- Run Self Supervised Model script ---
-python tstcc_train.py run \
+# python tstcc_train.py run \
+#   --mlflow_tracking_uri "http://fs0.das6.cs.vu.nl:5005" \
+#   --window_data_path "../../../../var/scratch/cla224/ECG-Project/data/windowed_data.h5" \
+#   --tcc_epochs 100 \
+#   --label_fraction 1.0 \
+#   --cc_temperature 0.07 \
+#   --tc_timesteps 70 \
+#   --tcc_batch_size 128 \
+#   --tc_hidden_dim 128
+
+python tstcc_soft_train.py run \
   --mlflow_tracking_uri "http://fs0.das6.cs.vu.nl:5005" \
   --window_data_path "../../../../var/scratch/cla224/ECG-Project/data/windowed_data.h5" \
-  --tcc_epochs 100 \
+  --tcc_epochs 3 \
   --label_fraction 1.0 \
-  --cc_temperature 0.07 \
-  --tc_timesteps 70 \
-  --tcc_batch_size 128 \
-  --tc_hidden_dim 128
