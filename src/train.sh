74888bec716bd95097bd14bcaaf2f0f21282fade
Cibber-2 <carlos.laborda.g@gmail.com>
2025-05-27 20:23:10 +0200

    update ts2vec_epochs parameter in training script to 5

--- src/train.sh
+++ src/train.sh
@@ -67,5 +67,5 @@ echo "Launching training with seed: $1"
 python ts2vec_soft_train_cv.py run \
   --mlflow_tracking_uri "http://fs0.das6.cs.vu.nl:5005" \
   --window_data_path "../../../../var/scratch/cla224/ECG-Project/data/windowed_data.h5" \
-  --ts2vec_epochs 1 \
+  --ts2vec_epochs 5 \
   --seed $1
