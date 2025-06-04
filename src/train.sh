a892b070d65ca2547a54db7b01a2536c177d6c4e
Cibber-2 <carlos.laborda.g@gmail.com>
2025-06-04 08:58:24 +0200

    refactor training scripts: update model commands and add simclr training

--- src/train.sh
+++ src/train.sh
@@ -20,49 +20,86 @@ source activate /var/scratch/cla224/ECG_env
 export MLFLOW_TRACKING_URI=http://fs0.das6.cs.vu.nl:5005
 
 # --- Run Fully Supervised Model script ---
+# CNN
 # echo "Launching training with seed: $1"
-python supervised_training.py run \
-  --mlflow_tracking_uri "http://fs0.das6.cs.vu.nl:5005" \
-  --window_data_path "../../../../var/scratch/cla224/ECG-Project/data/windowed_data.h5" \
-  --model_type "transformer" \
-  --seed $1 \
-  --lr 1e-5 \
-  --patience 20 \
-  # --scheduler_factor 0.1 \
-
-  
-
-# echo "Launching training with seed: $1"
-# python tstcc_train.py run \
+# python supervised_training.py run \
 #   --mlflow_tracking_uri "http://fs0.das6.cs.vu.nl:5005" \
 #   --window_data_path "../../../../var/scratch/cla224/ECG-Project/data/windowed_data.h5" \
-#   --tcc_epochs 40 \
-#   --seed $1
-#   --label_fraction 1.0 \
-#   --cc_temperature 0.07 \
-#   --tc_timesteps 70 \
-#   --tc_hidden_dim 128 \
-#   --tau_inst 10 \
-#   --lambda_aux 0.2 \
-#   --tau_temp 1 \
-  
+#   --model_type "cnn" \
+#   --seed $1 \
+#   --batch_size 16 \
+#   --scheduler_factor 0.5 \
+#   --scheduler_min_lr 1e-09 \
+
+# TCN
 # echo "Launching training with seed: $1"
-# python simclr_train.py run \
+# python supervised_training.py run \
 #   --mlflow_tracking_uri "http://fs0.das6.cs.vu.nl:5005" \
 #   --window_data_path "../../../../var/scratch/cla224/ECG-Project/data/windowed_data.h5" \
-#   --epochs 100 \
-#   --label_fraction 1.0 \
-#   --seed $1
+#   --model_type "tcn" \
+#   --seed $1 \
+#   --lr 0.0005 \
+#   --patience 20 \
 
-# Transfer learning
-# python ppg_ts2vecsoft_train.py run \
+# Transformer
+# echo "Launching training with seed: $1"
+# python supervised_training.py run \
 #   --mlflow_tracking_uri "http://fs0.das6.cs.vu.nl:5005" \
-#   --ecg_window_path "../../../../var/scratch/cla224/ECG-Project/data/windowed_data.h5" \
-#   --ppg_window_path "../../../../var/scratch/cla224/ECG-Project/data/ppg_windows.h5" \
+#   --window_data_path "../../../../var/scratch/cla224/ECG-Project/data/windowed_data.h5" \
+#   --model_type "transformer" \
+#   --seed $1 \
+#   --lr 1e-5 \
+#   --patience 20 \
 
+# --- Run Self Supervised Model script ---
+# TS2Vec
 # echo "Launching training with seed: $1"
 # python ts2vec_train.py run \
 #   --mlflow_tracking_uri "http://fs0.das6.cs.vu.nl:5005" \
 #   --window_data_path "../../../../var/scratch/cla224/ECG-Project/data/windowed_data.h5" \
 #   --ts2vec_epochs 5 \
+#   --label_fraction 1 \
 #   --seed $1
+
+# TS2VecSoft
+# echo "Launching training with seed: $1"
+# python ts2vec_soft_train_1.py run \
+#   --mlflow_tracking_uri "http://fs0.das6.cs.vu.nl:5005" \
+#   --window_data_path "../../../../var/scratch/cla224/ECG-Project/data/windowed_data.h5" \
+#   --ts2vec_epochs 5 \
+#   --label_fraction 1 \
+#   --seed $1
+
+# TSTCC
+# echo "Launching training with seed: $1"
+# python tstcc_train.py run \
+#   --mlflow_tracking_uri "http://fs0.das6.cs.vu.nl:5005" \
+#   --window_data_path "../../../../var/scratch/cla224/ECG-Project/data/windowed_data.h5" \
+#   --tcc_epochs 40 \
+#   --label_fraction 1.0 \
+#   --seed $1
+
+# TSTCCSoft
+# echo "Launching training with seed: $1"
+# python tstcc_soft_train_1.py run \
+#   --mlflow_tracking_uri "http://fs0.das6.cs.vu.nl:5005" \
+#   --window_data_path "../../../../var/scratch/cla224/ECG-Project/data/windowed_data.h5" \
+#   --tcc_epochs 40 \
+#   --label_fraction 1.0 \
+#   --seed $1
+  
+# echo "Launching training with seed: $1"
+python simclr_train.py run \
+  --mlflow_tracking_uri "http://fs0.das6.cs.vu.nl:5005" \
+  --window_data_path "../../../../var/scratch/cla224/ECG-Project/data/windowed_data.h5" \
+  --epochs 300 \
+  --label_fraction 1.0 \
+  --seed $1
+
+
+# --- Run Transfer Learning script ---
+# Transfer learning
+# python ppg_ts2vecsoft_train.py run \
+#   --mlflow_tracking_uri "http://fs0.das6.cs.vu.nl:5005" \
+#   --ecg_window_path "../../../../var/scratch/cla224/ECG-Project/data/windowed_data.h5" \
+#   --ppg_window_path "../../../../var/scratch/cla224/ECG-Project/data/ppg_windows.h5" \
